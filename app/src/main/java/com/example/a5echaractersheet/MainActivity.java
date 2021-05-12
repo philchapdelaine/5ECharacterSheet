eb14d6d81ff5ac9650661025d81de7c4718a0ce8
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2021-05-11 18:26:34 -0700

    init

---
+++ app/src/main/java/com/example/a5echaractersheet/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.a5echaractersheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager2);
+        FragmentManager fm = getSupportFragmentManager();
+
+        fragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager2.setAdapter(fragmentAdapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("main"));
+        tabLayout.addTab(tabLayout.newTab().setText("skills"));
+        tabLayout.addTab(tabLayout.newTab().setText("spells"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+}
